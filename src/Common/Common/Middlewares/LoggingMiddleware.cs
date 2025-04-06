3317f166cec616ccaae5f96d4e192ecb00d779b2
Ekin Bulut <ekinbulut@gmail.com>
2025-04-06 22:35:44 +0300

    Update RabbitMQ hostname in development settings and simplify logging middleware

--- src/Common/Common/Middlewares/LoggingMiddleware.cs
+++ src/Common/Common/Middlewares/LoggingMiddleware.cs
@@ -18,18 +18,18 @@ public class LoggingMiddleware
     public async Task Invoke(HttpContext context)
     {
         //get correlation id from request header
-        var correlationId = context.Request.Headers["X-Correlation-ID"].FirstOrDefault();
+        // var correlationId = context.Request.Headers["X-Correlation-ID"].FirstOrDefault();
         var stopwatch = Stopwatch.StartNew();
             
         // Log details about the incoming request.
-        _logger.LogInformation("Handling request: {CorrelationId} {Method} {Path}", correlationId, context.Request.Method, context.Request.Path);
+        _logger.LogInformation("Handling request: {Method} {Path}", context.Request.Method, context.Request.Path);
             
         await _next(context);
             
         stopwatch.Stop();
             
         // Log details about the response after processing.
-        _logger.LogInformation("Finished handling request: {CorrelationId} correlationId {Method} {Path} responded {StatusCode} in {ElapsedMilliseconds}ms",
+        _logger.LogInformation("Finished handling request: {Method} {Path} responded {StatusCode} in {ElapsedMilliseconds}ms",
             context.Request.Method, context.Request.Path, context.Response.StatusCode, stopwatch.ElapsedMilliseconds);
     }
 }
