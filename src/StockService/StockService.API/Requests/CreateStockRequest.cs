e8114c55f412837860e03ce6354c03be71fcc368
ekinbulut <ekinbulut@gmail.com>
2025-04-11 13:22:08 +0300

    Add Name field to stock-related commands and queries

--- src/StockService/StockService.API/Requests/CreateStockRequest.cs
+++ src/StockService/StockService.API/Requests/CreateStockRequest.cs
@@ -7,4 +7,6 @@ public class CreateStockRequest
     public Guid ProductId { get; set; }
     public int Quantity { get; set; }
     public decimal Price { get; set; }
+    public string Name { get; set; }
+    
 }
--- src/StockService/StockService.Application/Commands/CreateStockCommand.cs
+++ src/StockService/StockService.Application/Commands/CreateStockCommand.cs
@@ -7,4 +7,5 @@ public class CreateStockCommand : IRequest, IRequest<Guid>
     public Guid ProductId { get; set; }
     public int Quantity { get; set; }
     public decimal Price { get; set; }
+    public string Name { get; set; }
 }
